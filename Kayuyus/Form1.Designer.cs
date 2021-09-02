cf87c99ad572716ebddc3b92490690b727728a25
Yeghon <haronyegon90@gmail.com>
2021-09-02 14:44:16 +0300

    🚀 Create a windows from application that connects local ms sql database instance and display ward records filtered to 5 by default otherwise you can sarch by a value

---
+++ Kayuyus/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Kayuyus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelWard = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbwardName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.AliceBlue;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(86, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(635, 176);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // labelWard
+            // 
+            this.labelWard.AutoSize = true;
+            this.labelWard.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.labelWard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelWard.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWard.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelWard.Location = new System.Drawing.Point(0, 0);
+            this.labelWard.Name = "labelWard";
+            this.labelWard.Size = new System.Drawing.Size(55, 17);
+            this.labelWard.TabIndex = 1;
+            this.labelWard.Text = "WARDS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbwardName
+            // 
+            this.tbwardName.Location = new System.Drawing.Point(246, 61);
+            this.tbwardName.Name = "tbwardName";
+            this.tbwardName.Size = new System.Drawing.Size(243, 20);
+            this.tbwardName.TabIndex = 3;
+            this.tbwardName.TextChanged += new System.EventHandler(this.tbwardName_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(501, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbwardName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelWard);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelWard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbwardName;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Kayuyus/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kayuyus
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        string connectionString = ("Server=mc-ict-53;Database=expiry_registration;User=sa;Password=20304wW.;");
+        string searchValue = "";
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(connectionString);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "select top 5 * from tb_ward";
+                if (searchValue != "")
+                {
+                    // check if number
+                    if (Regex.IsMatch(searchValue.Trim(), @"^\d+$"))
+                    {
+                        query = $"select * from tb_ward where ward_id = {searchValue}";
+                    }
+                    else
+                    {
+                        query = $"select * from tb_ward where ward_name = '{searchValue}'";
+                    }
+                }
+                Console.WriteLine($"Query {query}");
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            catch
+            {
+                MessageBox.Show("Failed");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
+        }
+
+        private void tbwardName_TextChanged(object sender, EventArgs e)
+        {
+            searchValue = tbwardName.Text;
+        }
+    }
+}
